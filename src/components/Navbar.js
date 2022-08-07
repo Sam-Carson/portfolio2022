04dd127da96cd34b2b4412472e72b784046f89bd
samcarson <carsonsam4@gmail.com>
2022-08-07 15:09:34 -0700

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,40 @@ import "../styles/Navbar.css";
 export default function navbar() {
   return (
     <div className="navbar-container">
-      <a href="/home" className="navbar-link">
-        Home
-      </a>
-      <a href="/projects" className="navbar-link">
-        Projects
-      </a>
-      <a href="/skills" className="navbar-link">
-        Skills
-      </a>
-      <a href="/aboutme" className="navbar-link">
-        About Me
-      </a>
-      <a href="/resume" className="navbar-link">
-        Resume
-      </a>
+      <div href="/home" className="logo-container">
+        <div className="logo"></div>
+        <p id="logo-smdc">smdc.</p>
+      </div>
+      <div className="navbar-links-container">
+        <a href="/home" className="navbar-link">
+          Home
+        </a>
+        <span className="navbar-link-spacer">|</span>
+        <a href="/projects" className="navbar-link">
+          Works
+        </a>
+        <span className="navbar-link-spacer">|</span>
+
+        <a href="/skills" className="navbar-link">
+          Skills
+        </a>
+        <span className="navbar-link-spacer">|</span>
+
+        <a href="/aboutme" className="navbar-link">
+          About
+        </a>
+        <span className="navbar-link-spacer">|</span>
+        <a href="/contact" className="navbar-link">
+          Contact
+        </a>
+      </div>
+      <div className="resume-container">
+        <div className="resume-btn">
+          <a href="/resume" className="navbar-link resume-link">
+            Resume
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
