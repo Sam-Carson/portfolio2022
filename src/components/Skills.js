2e93795441be90e2276ef7f81936de9421c763dd
samcarson <carsonsam4@gmail.com>
2022-08-14 23:57:57 -0700

    Worked on skills section for mobile

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,23 +5,25 @@ export default function Skills() {
   return (
     <section>
       <div className="skills-container">
-        <div className="header-container">
+        <div className="skills-header">
           <h3>Skills</h3>
         </div>
         <div className="skills-body">
           <div className="skill-box">
-            <p>Front-end Development</p>
-            <p>HTML5, CSS3(SASS), JavaScript, React</p>
+            <p className="skillset-header">Front-end Development</p>
+            <p className="skillset-p">HTML5, CSS3, SASS, JavaScript, React</p>
           </div>
           <div className="skill-box">
-            <p>Back-end Development</p>
+            <p className="skillset-header">Back-end Development</p>
+            <p className="skillset-p">Node.JS, Express</p>
           </div>
           <div className="skill-box">
-            <p>Data Access</p>
+            <p className="skillset-header">Data Access</p>
+            <p className="skillset-p">SQL</p>
           </div>
           <div className="skill-box">
-            <p>Languages</p>
-            <p>JavaScript, C#, Java</p>
+            <p className="skillset-header">Languages</p>
+            <p className="skillset-p">C#, Java</p>
           </div>
         </div>
       </div>
