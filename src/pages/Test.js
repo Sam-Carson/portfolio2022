f0cd605ad05fce1e5fc0a6419773b16df9d71b0e
samcarson <carsonsam4@gmail.com>
2022-08-15 19:18:35 -0700

    working on navbar mobile, added glassmorphism

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Test() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
